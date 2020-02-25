dd13d17d57b3d5f3ce49b13db1a8215f1c802107
Tanishq Gupta <tanishqg2406@gmail.com>
2020-02-26 00:08:50 +0530

    in progress

--- A2/rsa.cpp
+++ A2/rsa.cpp
@@ -28,6 +28,26 @@ mpz_class generateStrongPrime() {
     return p;
 }
 
+string encryptVignere(string plainText, string key) {
+    string cipherText = ""; int m_size = plainText.size(), k_size = key.size();
+    char c;
+    for(int i=0;i<m_size;i++) {
+        c = (plainText[i] - 'a' + key[i % k_size] - 'a') % 26 + 'a';
+        cipherText += c;
+    }
+    return cipherText;
+}
+
+string decryptVignere(string cipherText, string key) {
+    string plainText = ""; int c_size = cipherText.size(), k_size = key.size();
+    char c;
+    for(int i=0;i<c_size;i++) {
+        c = (cipherText[i] - key[i % k_size] + 26) % 26 + 'a';
+        plainText += c;
+    }
+    return plainText;
+}
+
 class publicKey {
     mpz_class n, e;
 public:
@@ -36,30 +56,51 @@ public:
         this->n = n;
         this->e = e;
     }
-    publicKey(const publicKey& pk) {
-        this->n = pk.get_n();
-        this->e = pk.get_e();
-    }
     mpz_class get_n() const {return n;}
     mpz_class get_e() const {return e;}
 };
 
+class secretKey {
+    mpz_class p, q, d;
+public:
+    secretKey() {}
+    secretKey(mpz_class p, mpz_class q, mpz_class d) {
+        this->p = p;
+        this->q = q;
+        this->d = d;
+    }
+    mpz_class get_p() const {return p;}
+    mpz_class get_q() const {return q;}
+    mpz_class get_d() const {return d;}
+    mpz_class power(mpz_class) const;
+};
+
+mpz_class secretKey::power(mpz_class m) const {
+    mpz_class mp = m % p, mq = m % q;
+    mpz_powm(mp.get_mpz_t(), mp.get_mpz_t(), d.get_mpz_t(), p.get_mpz_t());
+    mpz_powm(mq.get_mpz_t(), mq.get_mpz_t(), d.get_mpz_t(), q.get_mpz_t());
+    mpz_class p_1, q_1;
+    mpz_invert(p_1.get_mpz_t(), p.get_mpz_t(), q.get_mpz_t());
+    mpz_invert(q_1.get_mpz_t(), q.get_mpz_t(), p.get_mpz_t());
+    return (mp * q * q_1 + mq * p * p_1) % (p * q);
+}
+
 class User {
     int id;         // identity of user
     publicKey pk;   // public key
-    mpz_class sk;   // secret key
+    secretKey sk;   // secret key
 
-    pair<publicKey, mpz_class> generateKey();
+    pair<publicKey, secretKey> generateKey();
 public:
     User(int);
     int getID();
     publicKey getPublicKey();
-    mpz_class encrypt(string);
-    string decrypt(mpz_class); 
+    string encrypt(string, User);
+    string decrypt(string, User); 
 };
 
 
-class ca {
+class CertificateAuthority {
     map<int, publicKey> table;
 public:
     void registerUser(User u);
@@ -67,7 +108,7 @@ public:
 } CA;
 
 
-void ca::registerUser(User u) {
+void CertificateAuthority::registerUser(User u) {
     if(table.find(u.getID()) != table.end()) {
         throw "This ID is already with another user !";
     }
@@ -76,7 +117,7 @@ void ca::registerUser(User u) {
     }
 }
 
-publicKey ca::getPublicKeyOfUser(int id) {
+publicKey CertificateAuthority::getPublicKeyOfUser(int id) {
     if(table.find(id) == table.end()) {
         throw "No such user !";
     }
@@ -85,19 +126,19 @@ publicKey ca::getPublicKeyOfUser(int id) {
     }
 }
 
-pair<publicKey, mpz_class> User::generateKey() {
+pair<publicKey, secretKey> User::generateKey() {
     mpz_class p = generateStrongPrime(), q = generateStrongPrime();
-    // cout << p << "\n" << q << "\n";
+    // insert a while loop till abs(p-q) < threshold
     mpz_class n = p * q, phi_n = (p - 1) * (q - 1), d, e;
     mpz_urandomb(e.get_mpz_t(), state, 50);
     while(gcd(e, phi_n) != 1) mpz_urandomb(e.get_mpz_t(), state, 50);
     mpz_invert(d.get_mpz_t(), e.get_mpz_t(), phi_n.get_mpz_t());
-    return {publicKey(n, e), d};
+    return {publicKey(n, e), secretKey(p, q, d)};
 }
 
 User::User(int id) {
     this->id = id;
-    pair<publicKey, mpz_class> key = generateKey();
+    pair<publicKey, secretKey> key = generateKey();
     this->pk = key.first;
     this->sk = key.second;
     CA.registerUser(*this);
@@ -106,6 +147,12 @@ User::User(int id) {
 int User::getID() {return id;}
 
 publicKey User::getPublicKey() {return pk;}
+
+
+
+// string User::encrypt(string m, User b) {
+
+// }
 
 int main() {
     gmp_randinit_mt(state);
@@ -122,9 +169,9 @@ int main() {
     // cout<<k.get_e()<<"\n";
     User a(123), b(256);
     publicKey pka = a.getPublicKey(), pkb = b.getPublicKey();
-    cout<<pka.get_n()<<"\n";
-    cout<<pka.get_e()<<"\n";
-    cout<<pkb.get_n()<<"\n";
-    cout<<pkb.get_e()<<"\n";
+    // cout<<pka.get_n()<<"\n";
+    // cout<<pka.get_e()<<"\n";
+    // cout<<pkb.get_n()<<"\n";
+    // cout<<pkb.get_e()<<"\n";
     return 0;
 }
