1d1ecb8625bc688e8561dc30dd406838f91e6123
Tanishq Gupta <tanishqg2406@gmail.com>
2020-02-26 23:40:52 +0530

    signature not working

--- A2/rsa.cpp
+++ A2/rsa.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 #include <gmpxx.h>
 
+/*
+ * TODO:
+ * integrate vignere cipher
+ * CA
+ * Larger Primes
+ */
+
 using namespace std;
 
 gmp_randstate_t state;
@@ -18,13 +25,13 @@ mpz_class generateStrongPrime() {
     s = generate_prime(100); t = generate_prime(100);
     mpz_urandomb(i.get_mpz_t(), state, 50);
     r = 2 * i * t + 1;
-    while(mpz_probab_prime_p(r.get_mpz_t(), 50) == 0) r += t<<1;
-    e = r-2;
-    mpz_powm (p0.get_mpz_t(), s.get_mpz_t(), e.get_mpz_t(), r.get_mpz_t());
+    while(mpz_probab_prime_p(r.get_mpz_t(), 50) == 0) r += 2 * t;
+    e = r - 2;
+    mpz_powm(p0.get_mpz_t(), s.get_mpz_t(), e.get_mpz_t(), r.get_mpz_t());
     p0 = 2 * p0 * s - 1;
     mpz_urandomb(j.get_mpz_t(), state, 50);
     p = p0 + 2 * j * r * s;
-    while(mpz_probab_prime_p(p.get_mpz_t(), 50) == 0) p += (r * s)<<1;
+    while(mpz_probab_prime_p(p.get_mpz_t(), 50) == 0) p += 2 * r * s;
     return p;
 }
 
@@ -58,6 +65,7 @@ public:
     }
     mpz_class get_n() const {return n;}
     mpz_class get_e() const {return e;}
+    string encrypt(string) const;
 };
 
 class secretKey {
@@ -72,10 +80,11 @@ public:
     mpz_class get_p() const {return p;}
     mpz_class get_q() const {return q;}
     mpz_class get_d() const {return d;}
-    mpz_class power(mpz_class) const;
+    mpz_class powerCRT(mpz_class) const;
+    string decrypt(string) const;
 };
 
-mpz_class secretKey::power(mpz_class m) const {
+mpz_class secretKey::powerCRT(mpz_class m) const {
     mpz_class mp = m % p, mq = m % q;
     mpz_powm(mp.get_mpz_t(), mp.get_mpz_t(), d.get_mpz_t(), p.get_mpz_t());
     mpz_powm(mq.get_mpz_t(), mq.get_mpz_t(), d.get_mpz_t(), q.get_mpz_t());
@@ -85,18 +94,70 @@ mpz_class secretKey::power(mpz_class m) const {
     return (mp * q * q_1 + mq * p * p_1) % (p * q);
 }
 
+int getBlockSize(mpz_class n) {
+    int r = 0;
+    mpz_class ans = 1;
+    while(ans < n) {ans *= 26; r++;}
+    return r-2;
+}
+
+string publicKey::encrypt(string plainText) const {
+    int blockSize = getBlockSize(n);
+    string cipherText = "";
+    for(int i=0;i<plainText.size();i+=blockSize) {
+        mpz_class M = 0, tmp;
+        for(int j=i;j<i+blockSize;j++) {
+            M *= 26;
+            if(j < plainText.size()) M += plainText[j] - 'a';
+            else M += 'x' - 'a';
+        }
+        assert(M < n);
+        mpz_powm(M.get_mpz_t(), M.get_mpz_t(), e.get_mpz_t(), n.get_mpz_t());
+        for(int j=0;j<=blockSize;j++) {
+            tmp = M % 26;
+            cipherText += 'a' + mpz_get_ui(tmp.get_mpz_t());
+            M /= 26;
+        }
+    }
+    return cipherText;
+}
+
+string secretKey::decrypt(string cipherText) const {
+    mpz_class n = p * q;
+    int blockSize = getBlockSize(n) + 1;
+    string plainText = "";
+    for(int i=0;i<cipherText.size();i+=blockSize) {
+        mpz_class M = 0, pow_26 = 1, tmp;
+        for(int j=i;j<i+blockSize;j++) {
+            if(j < cipherText.size()) M += pow_26 * (cipherText[j] - 'a');
+            else M += pow_26 * ('x' - 'a');
+            pow_26 *= 26;
+        }
+        assert(M < n);
+        M = powerCRT(M);
+        string rev = "";
+        for(int j=0;j<blockSize-1;j++) {
+            tmp = M % 26;
+            rev += 'a' + mpz_get_ui(tmp.get_mpz_t());
+            M /= 26;
+        }
+        reverse(rev.begin(), rev.end());
+        plainText += rev;
+    }
+    return plainText;
+}
+
 class User {
     int id;         // identity of user
     publicKey pk;   // public key
     secretKey sk;   // secret key
-
     pair<publicKey, secretKey> generateKey();
 public:
     User(int);
-    int getID();
-    publicKey getPublicKey();
-    string encrypt(string, User);
-    string decrypt(string, User); 
+    int getID() const;
+    publicKey getPublicKey() const;
+    string encrypt(string, User) const;
+    string decrypt(string, User) const; 
 };
 
 
@@ -144,34 +205,29 @@ User::User(int id) {
     CA.registerUser(*this);
 }
 
-int User::getID() {return id;}
+int User::getID() const {return id;}
 
-publicKey User::getPublicKey() {return pk;}
+publicKey User::getPublicKey() const {return pk;}
 
+string User::encrypt(string message, User b) const {
+    return b.getPublicKey().encrypt(sk.decrypt(message));
+    // return b.getPublicKey().encrypt(message);
+}
 
-
-// string User::encrypt(string m, User b) {
-
-// }
+string User::decrypt(string message, User a) const {
+    return a.getPublicKey().encrypt(sk.decrypt(message));
+    // return sk.decrypt(message);
+}
 
 int main() {
     gmp_randinit_mt(state);
-    gmp_randseed_ui(state, time(0));
-    // mpz_class p = generate_prime(100);
-    // // cout<<p<<endl;
-    // mpz_class p0 = generateStrongPrime();
-    // // cout<<p0<<"\n";
-    // // cout<<p0.get_str().size()<<"\n";
-    // publicKey k(p, p0);
-    // cout<<p<<"\n";
-    // cout<<k.get_n()<<"\n";
-    // cout<<p0<<"\n";
-    // cout<<k.get_e()<<"\n";
+    gmp_randseed_ui(state, 0);
     User a(123), b(256);
-    publicKey pka = a.getPublicKey(), pkb = b.getPublicKey();
-    // cout<<pka.get_n()<<"\n";
-    // cout<<pka.get_e()<<"\n";
-    // cout<<pkb.get_n()<<"\n";
-    // cout<<pkb.get_e()<<"\n";
+    // publicKey pka(55, 3); secretKey ska(11, 5, 27);
+    // publicKey pkb(35, 5); secretKey skb(5, 7, 5);
+    // a.pk = pka; a.sk = ska;
+    // b.pk = pkb; b.sk = skb; 
+    string c = a.encrypt("tanishqissuperhot", b);
+    cout<<b.decrypt(c, a)<<"\n";
     return 0;
 }
